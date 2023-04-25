f9a8a9c5c5982895a60e8a246e7fbacd29b28218
l3miage-delagral <alex.del57640@gmail.com>
2023-04-25 16:43:36 +0200

    connexion mail ok

--- front/Miahoot/src/app/app-routing.module.ts
+++ front/Miahoot/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ConnexionAnonymeComponent } from './connexion-anonyme/connexion-anonyme.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 const routes: Routes = [
   {path:"", component: AccueilComponent},
-  {path:"connexionAnonyme",component: ConnexionAnonymeComponent}
+  {path:"connexionAnonyme",component: ConnexionAnonymeComponent},
+  {path:"connexion", component: ConnexionComponent}
 ];
 
 @NgModule({
--- front/Miahoot/src/app/connexion/connexion.component.ts
+++ front/Miahoot/src/app/connexion/connexion.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ConnexionService } from '../services/connexion.service';
+import { MiahootUser } from '../miahoot';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent {
+  user !: MiahootUser;
 
+
+  public fg: FormGroup<{
+    name: FormControl<string>,
+    email: FormControl<string>
+    password: FormControl<string>
+  }>;
+
+  constructor(private connexionServ: ConnexionService,
+    private fb: FormBuilder) {
+      this.connexionServ.obsMiahootConcepteur$.subscribe(
+        u => {
+          if( u === undefined){
+          this.fg.controls.name.setValue("")
+          this.fg.controls.email.setValue("")
+          this.fg.controls.password.setValue("")
+        } else {
+          this.fg.controls.name.setValue(u.name)
+          this.fg.controls.email.setValue(u.email)
+        }
+      }
+      )
+
+    this.fg = fb.nonNullable.group({
+      name: [""],
+      email: [""],
+      password: [""]
+    })
+
+  }
+
+  login() {
+    this.connexionServ.login(this.fg.controls.email.value, this.fg.controls.password.value)
+  }
+
+  register(){
+    this.connexionServ.register(this.fg.controls.email.value, this.fg.controls.password.value)
+  }
 }
