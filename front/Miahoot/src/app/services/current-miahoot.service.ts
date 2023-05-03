2f21ae3535470e1c23c1252d9c2324e1870b9d71
l3miage-delagral <alex.del57640@gmail.com>
2023-05-03 11:12:16 +0200

    V0 OP

--- front/Miahoot/src/app/services/current-miahoot.service.ts
+++ front/Miahoot/src/app/services/current-miahoot.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, combineLatest, map, of, switchMap } from 'rxjs';
-import { FsMiahootProjectedConverter, FsQCMProjectedConverter, MiahootProjected, QCMProjected, conv } from '../miahoot';
-import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable, combineLatest, lastValueFrom, map, of, switchMap, take, tap } from 'rxjs';
+import { FsMiahootProjectedConverter, FsQCMProjectedConverter, MiahootProjected, QCMProjected, Question, VOTES, conv } from '../miahoot';
+import { Firestore, addDoc, collection, doc, docData, setDoc, updateDoc } from '@angular/fire/firestore';
 import { Auth, authState } from '@angular/fire/auth';
 
 @Injectable({
@@ -68,4 +68,34 @@ export class CurrentMiahootService {
     )
     )
   }
+
+  async ajouterQuestion(question : Question){
+    console.log("je suis la");
+    
+    this.obsProjectedMiahootID.pipe(
+      take(1),
+      map(async MiahootId => {
+          if (MiahootId) {
+            const QMCsCollection =  collection(this.fs,`/miahoot/${MiahootId}/QCMs`)
+            const jsp = await addDoc(QMCsCollection,{
+              question : question.label,
+              responses : question.reponses.map( value => value.label),
+              correctAnswer: question.reponses.findIndex(value => value.estValide === true),
+              votes:[] as VOTES[]
+            })
+            const miahootActuel = doc(this.fs,`miahoot/${MiahootId}`)
+            await updateDoc(miahootActuel,{
+              currentQCM : jsp.id
+            })
+          }
+        }
+        )
+    ).subscribe()
+
+
+
+
+
+  }
+
 }
