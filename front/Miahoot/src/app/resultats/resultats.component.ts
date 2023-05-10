d6c18da671bda983ec7eda3ae7da21164a58b9ae
seifeddf <farahseifeddine@outlook.com>
2023-05-10 11:09:46 +0200

    idkkk

--- front/Miahoot/src/app/resultats/resultats.component.ts
+++ front/Miahoot/src/app/resultats/resultats.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CurrentMiahootService } from '../services/current-miahoot.service';
+import { CurrentMiahootService, RESULTATS } from '../services/current-miahoot.service';
 import { QCMProjected } from '../miahoot';
-import { Observable, map, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-resultats',
@@ -9,9 +10,32 @@ import { Observable, map, tap } from 'rxjs';
   styleUrls: ['./resultats.component.scss']
 })
 export class ResultatsComponent{
+  bsResultat = new BehaviorSubject<RESULTATS[]>([]);
 
-  constructor(private ms : CurrentMiahootService) { 
+  constructor(private ms : CurrentMiahootService, private router : Router) { 
+    this.ms.bsResultats.subscribe(this.bsResultat)
+    console.log("mon tableau", this.bsResultat.value)
+  }
+
+  // bestQuestion(): string {
+  //   // i want to  the question with the most number of votes for the correct answer
+  //   //  this.bsResultat.value.reduce(x, y =>
+  //   //   x if x.qcm.votes[x.qcm.correctAnswer] > y.qcm.votes[y.qcm.correctAnswer] else y).qcm.question
+
+  //   this.bsResultat.value.reduce((acc, value) => {
+  //     const n = Object.entries(value.qcm.votes).filter([key, val]=> val === value.qcm.correctanwser)
     
+  //     return 
+  //   },0)
+  // }
+
+  worstQuestion() { }
+
+  percentage() {}
+
+  resetResultats(){
+    this.bsResultat.next([])
+    this.router.navigateByUrl("miahootChoice")
   }
 
 
